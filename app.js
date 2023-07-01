11c857d91ea5f53526620d6215adfd15ab546a23
Ranjit Thota <amailtoranjith@gmail.com>
2023-07-01 15:47:26 +0200

    adding and deleting tasks

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const hbs = require("hbs");
 const path = require("path");
-const { readFileSync } = require("fs");
+const { readFileSync, writeFileSync } = require("fs");
+const bodyParser = require("body-parser");
+const { clear } = require("console");
 
 const hbsViewsPath = path.join(__dirname, "./templates/views");
 const hbsPartialsPath = path.join(__dirname, "./templates/partials");
@@ -10,6 +12,8 @@ const hbsPartialsPath = path.join(__dirname, "./templates/partials");
 // console.log(hbsViewsPath);
 
 const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 dotenv.config();
 
 app.use(express.static("public"));
@@ -19,10 +23,24 @@ hbs.registerPartials(hbsPartialsPath);
 app.set("views", hbsViewsPath);
 
 const tasks = JSON.parse(readFileSync("./data/data.json"));
-// console.log(tasks);
 
 app.get("/", (req, res) => {
   res.render("index.hbs", { pageTitle: "Todo List", loggedIn: false, tasks });
+});
+
+app.post("/addtask", (req, res) => {
+  const task = req.body.task;
+  console.log(task);
+  tasks.push(task);
+  writeFileSync("./data/data.json", JSON.stringify(tasks));
+  res.redirect("/");
+});
+app.post("/deletetask", (req, res) => {
+  const task = req.body.task;
+  console.log(task);
+  const newTasks = tasks.filter((item) => item !== task);
+  writeFileSync("./data/data.json", JSON.stringify(newTasks));
+  res.send({ link: "/" });
 });
 
 app.get("*", (req, res) => {
--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,39 @@
-console.log("works");
+const checkBtns = document.querySelectorAll(".check-btn");
+const deleteBtns = document.querySelectorAll(".delete-btn");
 
-const checkBtn = document.querySelector(".check-btn");
+checkBtns.forEach((checkBtn) => {
+  checkBtn.addEventListener("click", () => {
+    // console.log(checkBtn.parentElement.firstChild);
+    // currentTask = checkBtn.parentElement.firstChild.innerHTML;
+    currentTaskElement = checkBtn.parentElement.firstChild;
+    currentTaskElement.classList.toggle("completed");
+    checkBtn.innerHTML = checkBtn.innerHTML === "✓" ? "X" : "✓";
+  });
+});
 
-checkBtn.addEventListener("click", () => {
-  console.log("click works");
+deleteBtns.forEach((deleteBtn) => {
+  deleteBtn.addEventListener("click", () => {
+    currentTask = deleteBtn.parentElement.firstChild.innerHTML;
+
+    async function deleteTask() {
+      const response = await fetch("/deletetask", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ task: currentTask }),
+      });
+      // const json = await response.json();
+      // console.log(json);
+      const loading = document.createElement("span");
+      loading.classList.add("loading");
+      loading.innerHTML = "...loading";
+      currentTask = deleteBtn.parentElement.appendChild(loading);
+      setTimeout(() => {
+        window.location.href = "http://localhost:5000/";
+      }, 1000);
+    }
+
+    deleteTask();
+  });
 });
